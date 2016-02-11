f14465b6bf1f7bbae03efbd413ac2403db6a8821
StephanWagener <stephan-wagener@web.de>
2016-02-11 13:25:01 +0100

    Vorbereitungen für simmi und hollas.

--- app/src/main/java/com/simi/hftl_app/Fragmente/StudyInfoFragment.java
+++ app/src/main/java/com/simi/hftl_app/Fragmente/StudyInfoFragment.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 import com.simi.hftl_app.Main.MainActivity;
 import com.simi.hftl_app.Model.Category;
+import com.simi.hftl_app.Model.StudyCourse;
 import com.simi.hftl_app.R;
 import com.simi.hftl_app.Listen.StudyListItem;
 
@@ -50,6 +51,101 @@ public class StudyInfoFragment extends Fragment
             layout.setBackgroundColor(getActivity().getResources().getColor(R.color.study_job_color));
             titleJob.setTextColor(getActivity().getResources().getColor(R.color.study_job_color));
             titleCurri.setTextColor(getActivity().getResources().getColor(R.color.study_job_color));
+        }
+
+        TextView beginning = (TextView) view.findViewById(R.id.beginningText);
+        TextView perspectives = (TextView) view.findViewById(R.id.jobPerspectivesText);
+        TextView curriculumText = (TextView) view.findViewById(R.id.curriculumText);
+
+        if (element.getCourse().equals(StudyCourse.ICT_MASTER))
+        {
+            beginning.setText("ICT Master beginning Text");
+            perspectives.setText("ICT Master perspective Text.");
+            curriculumText.setText("ICT Master curriculum Text.");
+        }
+        else if (element.getCourse().equals(StudyCourse.KMI_BACHELOR))
+        {
+            beginning.setText("KMI BAchelor beginning Text");
+            perspectives.setText("KMI BAchelor perspective Text.");
+            curriculumText.setText("KMI BAchelor curriculum Text.");
+        }
+        else if (element.getCourse().equals(StudyCourse.DUAL_KMI_BACHELOR))
+        {
+            beginning.setText("");
+            perspectives.setText("");
+            curriculumText.setText("");
+        }
+        else if (element.getCourse().equals(StudyCourse.JOB_KMI_BACHELOR))
+        {
+            beginning.setText("");
+            perspectives.setText("");
+            curriculumText.setText("");
+        }
+        else if (element.getCourse().equals(StudyCourse.IKT_BACHELOR))
+        {
+            beginning.setText("");
+            perspectives.setText("");
+            curriculumText.setText("");
+        }
+        else if (element.getCourse().equals(StudyCourse.IKT_MASTER))
+        {
+            beginning.setText("");
+            perspectives.setText("");
+            curriculumText.setText("");
+        }
+        else if (element.getCourse().equals(StudyCourse.IKT_MASTER_ENGLISH))
+        {
+            beginning.setText("");
+            perspectives.setText("");
+            curriculumText.setText("");
+        }
+        else if (element.getCourse().equals(StudyCourse.JOB_IKT_BACHELOR))
+        {
+            beginning.setText("");
+            perspectives.setText("");
+            curriculumText.setText("");
+        }
+        else if (element.getCourse().equals(StudyCourse.JOB_IKT_MASTER))
+        {
+            beginning.setText("");
+            perspectives.setText("");
+            curriculumText.setText("");
+        }
+        else if (element.getCourse().equals(StudyCourse.WI_BACHELOR))
+        {
+            beginning.setText("");
+            perspectives.setText("");
+            curriculumText.setText("");
+        }
+        else if (element.getCourse().equals(StudyCourse.DUAL_WI_BACHELOR))
+        {
+            beginning.setText("");
+            perspectives.setText("");
+            curriculumText.setText("");
+        }
+        else if (element.getCourse().equals(StudyCourse.DUAL_WI_MASTER))
+        {
+            beginning.setText("");
+            perspectives.setText("");
+            curriculumText.setText("");
+        }
+        else if (element.getCourse().equals(StudyCourse.JOB_WI_BACHELOR))
+        {
+            beginning.setText("");
+            perspectives.setText("");
+            curriculumText.setText("");
+        }
+        else if (element.getCourse().equals(StudyCourse.JOB_WI_MASTER))
+        {
+            beginning.setText("");
+            perspectives.setText("");
+            curriculumText.setText("");
+        }
+        else if (element.getCourse().equals(StudyCourse.DUAL_AI_BACHELOR))
+        {
+            beginning.setText("");
+            perspectives.setText("");
+            curriculumText.setText("");
         }
 
         return view;
